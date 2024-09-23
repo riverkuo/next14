227c3768802612ecd4a3dc95fcebc8ca54e26136
River <coco0958231520@gmail.com>
2024-09-23 14:17:46 +0800

    feat. Clear main and install tailwind

--- src/app/searchCharas/[searchTerm]/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFound = () => {
-  return <div>Whoops... we can not find the character.</div>;
-};
-
-export default NotFound;
--- src/app/searchCharas/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SearchCharas = () => {
-  return <div>search page</div>;
-};
-
-export default SearchCharas;
